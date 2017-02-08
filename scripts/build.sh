bd91b4e704edd2d1891f6ca656dc8e4ef380a778
Balu Ramananda <balasubramanya_ramananda@mckinsey.com>
2017-02-08 18:05:47 +0530

    added android support

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,3 +4,7 @@ if [[ "$DEV" = "IOS" ]]; then
   xcodebuild -project $PWD/ios/AwesomeProject.xcodeproj -scheme AwesomeProject -configuration RELEASE OBJROOT=$PWD/ios/build SYMROOT=$PWD/ios/build -destination 'platform=iOS Simulator,name=iPhone 6,OS=9.3'
   xcodebuild archive -project $PWD/ios/AwesomeProject.xcodeproj -scheme AwesomeProject -configuration RELEASE -derivedDataPath $PWD/ios/build -archivePath $PWD/ios/build/Products/AwesomeProject.xcarchive
 fi
+
+if [[ "$DEV" = "ANDROID" ]]; then
+  $PWD/android/gradlew assembleRelease
+fi
